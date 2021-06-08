17cd4eafa361dddbd534a048191ba33d4ca4773c
Victor Chuol <victor.m.chuol@gmail.com>
2021-06-08 02:52:30 -0700

    Realtime-Database project read implementation

--- app/src/main/java/com/example/velvet/MainActivity.java
+++ app/src/main/java/com/example/velvet/MainActivity.java
@@ -19,6 +19,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.transition.Explode;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.TextureView;
@@ -31,16 +32,20 @@ import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.example.velvet.ProjectNameDialog.projectNameDialogListener;
 import org.w3c.dom.Text;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 /**
- * Implements a
  * @Author Victor Chuol
  *
  * **/
@@ -48,19 +53,48 @@ public class MainActivity extends AppCompatActivity implements ProjectNameDialog
     FirebaseAuth firebaseAuth;
     GoogleSignInClient signInClient;
     Button nextBtn; ScrollView scroll;
-    int count =0; GridLayout gridLayout;
-    TextView f;//UserSingleton singleton;
-    String projectString;static ArrayList<View> viewArrayList;
+    GridLayout gridLayout; UserSingleton singleton;
+    static ArrayList<View> viewArrayList;
 
     FirebaseDatabase rootNode = FirebaseDatabase.getInstance();
-    DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
-
+    DatabaseReference projectRef ;
     @Override
     protected void onStart(){
         super.onStart();
-        rootRef = rootNode.getReference("exampleSet");
-        //rootRef.setValue("User1");
-        //rootRef.child("")
+   }
+
+   /**
+    * Load Projects of user using firebase userID
+    * **/
+   protected void loadExistingProjects(FirebaseUser firebaseUser){
+        if(firebaseUser != null) {
+            projectRef = rootNode.getReference("projects2"); /**Testing Database**/
+            String TAG = "MainActivity";
+            String userID = firebaseUser.getUid();
+            projectRef.child(userID).addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    if( snapshot.exists()){
+                        for (DataSnapshot dataSnapshot: snapshot.getChildren()) {
+                            Button button = new Button(MainActivity.this);
+                            Project project = new Project();
+                            project.setName(dataSnapshot.child("name").getValue(String.class));
+                            project.setDayCreated(dataSnapshot.child("dateCreated").getValue(String.class));
+                            button.setText(project.getName());
+                            gridLayout.addView(button);
+       /**TESTING**/        Log.i(TAG, "onDataChange: snapshot procedure PASSED");
+                        }
+                    }else {
+       /**TESTING**/        Log.i(TAG, "onDataChange: snapshot does not exist " );
+                    }
+                }
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+
+        }
     }
 
     /**
@@ -150,29 +184,32 @@ public class MainActivity extends AppCompatActivity implements ProjectNameDialog
         //initialize firebase auth & user
         firebaseAuth = FirebaseAuth.getInstance();
         FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
-
-        if(firebaseUser != null) {
-            //read & asign firebase user info ex. image
-        }
-
         signInClient = GoogleSignIn.getClient(MainActivity.this, GoogleSignInOptions.DEFAULT_SIGN_IN);
 
-        UserSingleton singleton = new UserSingleton();
+        /**Initialize User singleton**/
+        //UserSingleton singleton = new UserSingleton();
+        singleton = new UserSingleton();
         singleton.getInstance().setAuth(firebaseAuth);
         singleton.getInstance().setGoogleSignInClient(signInClient);
 
+        //viewArrayList = new ArrayList<View>();
         viewArrayList = new ArrayList<View>();
+        loadExistingProjects(firebaseUser);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 try {
-                //Create dialog
+                /**Create dialog**/
                 addProjectDialog();
 
-                /*NEED TO STORE PROJECT NAME: MONGODB**/
-/** // Removed for testing purposes
+                /** TO-DO:
+                 *      ---> IMPLEMENT STORAGE OF PROJECT NAME
+                 *      IN FIREBASE WITHIN DIALOG FRAGMENT
+                 * **/
+
+/** UI-FOLDER-FEATURE Removed for testing purposes
                 ImageButton project_btn = new ImageButton(MainActivity.this);
                 project_btn.setBackgroundColor(0000);
                 project_btn.setImageResource(R.drawable.ic_folder2);
@@ -181,7 +218,7 @@ public class MainActivity extends AppCompatActivity implements ProjectNameDialog
                 pr_btn.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        //implements project page intent
+                        //implements INDIVIDUAL project page intent
                     }
                 });
                 viewArrayList.add(pr_btn);
@@ -199,7 +236,7 @@ public class MainActivity extends AppCompatActivity implements ProjectNameDialog
     }
 
     /**
-     * -->Dialog:
+     * -->ProjectNameDialog:
      * removes button when cancel selected
      * **/
     @Override
@@ -209,19 +246,18 @@ public class MainActivity extends AppCompatActivity implements ProjectNameDialog
         gridLayout.removeViewAt(s-1);
     }
     /**
-     * -->Dialog:
+     * -->ProjectNameDialog:
      * Apply text to Project Button
      * **/
     @Override
     public void applyTexts(String projectName) {
-        projectString = projectName;
         int s = viewArrayList.size();
         Button b =(Button) viewArrayList.get(s-1);
         b.setText(projectName);
         viewArrayList.add(s-1,b);
     }
     /**
-     * -->Dialog:
+     * -->ProjectNameDialog:
      * Creates project add Dialog
      * **/
     public void addProjectDialog(){
---
+++ app/src/main/java/com/example/velvet/Project.java
@@ -0,0 +1,57 @@
+package com.example.velvet;
+
+import java.util.HashMap;
+public class Project {
+    private String dayCreated;// private String mediaCluster;
+    private String name; private String timeStamp;
+    private HashMap<String,String> labels;
+    private HashMap<String,String> audio;
+
+    public Project() {
+        this.dayCreated = dayCreated;
+        this.name = name;
+        this.timeStamp = timeStamp;
+        this.labels = labels;
+        this.audio = audio;
+    }
+
+    public String getDayCreated() {
+        return dayCreated;
+    }
+
+    public void setDayCreated(String dayCreated) {
+        this.dayCreated = dayCreated;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(String timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public HashMap<String, String> getLabels() {
+        return labels;
+    }
+
+    public void setLabels(HashMap<String, String> labels) {
+        this.labels = labels;
+    }
+
+    public HashMap<String, String> getAudio() {
+        return audio;
+    }
+
+    public void setAudio(HashMap<String, String> audio) {
+        this.audio = audio;
+    }
+}
